3ce56da8f33d7a9d0df6b300b4272cb5a2ea0a89
dahstar <davood.abbaspour@gmail.com>
2018-07-01 19:33:27 +0430

    'dddd'

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
- int n;
- cin>>n;
- cout<<"number is"<<n;
+  int t;string s;
+vector<string> p;
+  cin>>t;
+  
+  for(int i=0;i<t;i++)
+  {
+   cin>>s;
+   p.push_back(s);
+  }
+    for(int i=p.size()-1;i>=0;i--)
+     cout<<p[i]<<" ";
+  
 }
-
-
-
-
-
 
